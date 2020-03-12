a87d6417d6502be27e7f0e8a7053401415bf7468
Paul Sitoh <paulwizviz@gmail.com>
2020-03-12 19:15:12 +0000

    Sort word combination
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ sort/wordcombination/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+)
+
+func rearrangeRune(input []rune) []string {
+
+	wordCombinations := []string{}
+
+	inputClone := make([]rune, len(input))
+	copy(inputClone, input)
+
+	indexOfFirstCharater := 0
+	for counter := 0; counter < len(input) - 1; counter++ {
+
+		if indexOfFirstCharater + 1 < len(input){
+			tmp := inputClone[indexOfFirstCharater]
+			inputClone[indexOfFirstCharater] = inputClone[indexOfFirstCharater + 1]
+			inputClone[indexOfFirstCharater + 1] = tmp
+			indexOfFirstCharater++
+		}
+
+		wordCombinations = append(wordCombinations, string(inputClone))
+	}
+	return wordCombinations
+}
+
+func main() {
+
+	data := "abc"
+	
+	wordCombinations := []string{data}
+	wordCombinations = append(wordCombinations, rearrangeRune([]rune(wordCombinations[0]))...)
+	log.Println(wordCombinations)
+
+	for count := 1; count < len(data); count++ {
+		wordCombinations = append(wordCombinations, rearrangeRune([]rune(wordCombinations[len(wordCombinations)-1]))...)
+		log.Println(wordCombinations)
+	}
+}
